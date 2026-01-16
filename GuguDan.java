fa0fca6b7346a2adf5ea4f985c2c3b3daa312cd9
ikaruswj <ikaruswj@gmail.com>
2026-01-16 14:25:30 +0900

    Change to show only 2-3 dan

--- GuguDan.java
+++ GuguDan.java
@@ -1,7 +1,7 @@
 public class GuguDan {
     public static void main(String[] args) {
-        // Multiplication table from 2 to 9
-        for (int i = 2; i <= 9; i++) {
+        // Multiplication table from 2 to 3
+        for (int i = 2; i <= 3; i++) {
             System.out.println("=== " + i + " dan ===");
             for (int j = 1; j <= 9; j++) {
                 System.out.println(i + " x " + j + " = " + (i * j));
